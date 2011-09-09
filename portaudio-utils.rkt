009e261e807002fed879a082b7bc4d37592ad529
John Clements <clements@racket-lang.org>
2011-09-09 14:16:03 -0700

    2 main callback styles tested

--- portaudio-utils.rkt
+++ portaudio-utils.rkt
@@ -9,10 +9,13 @@
        (<= 0 n)))
 
 (provide/contract 
- [make-copying-callback (-> cpointer? frames? channel? box? any)]
+ [make-copying-callback (-> s16vector? channel? box? any)]
+ ;; UNSAFE:
+ [make-copying-callback/cpointer (-> cpointer? frames? channel? box? any)]
  [make-generating-callback (-> procedure? frames? channel? box? any)]
  [make-block-generating-callback (-> procedure? channel? box? any)])
 
+;; all of these functions assume 2-channel-interleaved 16-bit input:
 (define channels 2)
 (define s16max 32767)
 (define s16-bytes 2)
@@ -24,46 +27,74 @@
   (thread (lambda () 
             (channel-put channel val))))
 
+;; create a callback that supplies frames from an s16vector.
+(define (make-copying-callback s16vec response-channel abort-box)
+  (define s16vec-len (s16vector-length s16vec))
+  (unless (= (modulo s16vec-len 2) 0)
+    (raise-type-error 'make-copying-callback
+                      "vector of length divisible by two"
+                      0
+                      s16vec
+                      response-channel
+                      abort-box))
+  (make-copying-callback/cpointer (s16vector->cpointer s16vec) 
+                                  (/ s16vec-len 2)
+                                  response-channel
+                                  abort-box))
+
 ;; create a callback that supplies frames from a buffer, using memcpy.
-(define (make-copying-callback master-buffer total-frames response-channel abort-box)
+;; UNSAFE! : give a very large value for total-frames and watch it play memory
+;; then crash.
+(define (make-copying-callback/cpointer master-buffer total-frames 
+                                        response-channel abort-box)
   (let* ([channels 2]
          [sample-offset 0] ;; mutable, to track through the buffer
          [total-samples (* total-frames channels)])
     (lambda (input output frame-count time-info status-flags user-data)
-      (cond [(unbox abort-box) 'pa-abort]
-            ;; MUST NOT ALLOW AN EXCEPTION TO ESCAPE.
-            [else (with-handlers ([(lambda (exn) #t)
-                                   (lambda (exn)
-                                     (channel-put/async response-channel exn)
-                                     'pa-abort)])
-                    (let ([buffer-samples (* frame-count channels)])
-                      (cond
-                        [(> (+ sample-offset buffer-samples) total-samples)
-                         (channel-put/async response-channel 'finished)
-                         ;; for now, just truncate if it doesn't come out even:
-                         ;; NB: all zero bits is the sint16 representation of 0
-                         (begin (memset output 0 buffer-samples _sint16)
-                                'pa-complete)]
-                        [else
-                         (begin (memcpy output
-                                        0
-                                        master-buffer
-                                        sample-offset
-                                        buffer-samples
-                                        _sint16)
-                                (set! sample-offset (+ sample-offset buffer-samples))
-                                'pa-continue)])))]))))
+      (cond [(unbox abort-box) 
+             (channel-put/async response-channel 'finished)
+             'pa-abort]
+            [else 
+             ;; the following code is believed not to be able to raise any errors.
+             ;; if this is wrong, racket will die with abort().
+             (let ([buffer-samples (* frame-count channels)])
+                    (cond
+                      [(> (+ sample-offset buffer-samples) total-samples)
+                       ;; for now, just truncate if it doesn't come out even:
+                       ;; NB: all zero bits is the sint16 representation of 0
+                       (memset output 0 buffer-samples _sint16)
+                       (channel-put/async response-channel 'finished)
+                       'pa-complete]
+                      [else
+                       (memcpy output
+                                     0
+                                     master-buffer
+                                     sample-offset
+                                     buffer-samples
+                                     _sint16)
+                       (set! sample-offset (+ sample-offset buffer-samples))
+                       'pa-continue]))]))))
 
 ;; create a callback that creates frames by calling a signal repeatedly.
 ;; note that we don't bother checking to see whether the buffer is successfully
 ;; filled.
 (define (make-generating-callback signal buffer-frames response-channel abort-box)
-  (define-values (filled-buffer semaphore filler-thread-abort) 
-    (start-filler-thread signal buffer-frames response-channel abort-box))
-  (define buffer-samples (* buffer-frames channels s16-bytes))
+  (define signal-exn-box (box #f))
+  (define-values (filled-buffer semaphore) 
+    (start-filler-thread signal buffer-frames signal-exn-box abort-box))
+  (define buffer-samples (* buffer-frames channels))
+  ;; allow the first buffer-fill to happen:
+  (semaphore-post semaphore)
   (lambda (input output frame-count time-info status-flags user-data)
+    ;; the following code is believed not to be able to raise any errors.
+    ;; if this is wrong, racket will die with abort().
+    
     ;; don't run if abort is set:
-    (cond [(unbox abort-box) 'pa-abort]
+    (cond [(unbox abort-box)
+           (define response
+             (or (unbox signal-exn-box) 'finished))
+           (channel-put/async response-channel response)
+           'pa-abort]
           ;; don't run if we get the wrong number of frames requested:
           [(not (= frame-count buffer-frames))
            (channel-put/async
@@ -90,9 +121,10 @@
 ;; this thread is not run as a callback. That way, if the user's signal
 ;; misbehaves, it won't destroy the audio engine. It fills 
 ;; the output buffer once for each post to the semaphore.
-(define (start-filler-thread signal buffer-frames response-channel abort-box)
+(define (start-filler-thread signal buffer-frames signal-exn-box abort-box)
   (define signal-semaphore (make-semaphore))
   (define buffer (make-s16vector (* channels buffer-frames)))
+  (define cpointer (s16vector->cpointer buffer))
   (define frame-offset 0)
   (thread
    (lambda ()
@@ -100,19 +132,20 @@
        (semaphore-wait signal-semaphore)
        (with-handlers ([(lambda (exn) #t)
                         (lambda (exn)
-                          (channel-put/async response-channel exn)
+                          (set-box! signal-exn-box exn)
                           (set-box! abort-box #t)
                           ;; fall off the end of the thread:
                           (void))])
          (for ([t (in-range frame-offset (+ frame-offset buffer-frames))]
                [i (in-range 0 (* 2 buffer-frames) 2)])
-           (let ([sample 
-                  (inexact->exact 
-                   (round (* s16max (min 1.0 (max -1.0 (signal t))))))])
-             (ptr-set! buffer _sint16 i sample)
-             (ptr-set! buffer _sint16 (+ i 1) sample)))
+           (define sample 
+             (inexact->exact 
+              (round (* s16max (min 1.0 (max -1.0 (signal t)))))))
+           (ptr-set! cpointer _sint16 i sample)
+           (ptr-set! cpointer _sint16 (+ i 1) sample))
          (set! frame-offset (+ frame-offset buffer-frames))
-         (loop))))))
+         (loop)))))
+  (values cpointer signal-semaphore))
 
 ;; create a callback that creates frames by passing a cblock to a function
 (define (make-block-generating-callback signal/block/s16 response-channel abort-box)
--- test-portaudio.rkt
+++ test-portaudio.rkt
@@ -11,6 +11,7 @@
 (run-tests
 (test-suite "portaudio"
 (let ()
+  
   (check-not-exn (lambda () (pa-get-version)))
   (check-not-exn (lambda () (pa-get-version-text)))
   
@@ -49,6 +50,8 @@
   
   ;; the remainder of these require 2-channel output
   
+  (pa-maybe-initialize)
+  
   (define (open-test-stream callback)
     (pa-open-default-stream
      0             ;; input channels
@@ -59,6 +62,17 @@
      callback ;; callback (NULL means just wait for data)
      #f))
   
+  (define (make-tone-buf pitch frames)
+    (define result (make-s16vector (* frames channels)))
+    (for ([i (in-range 0 frames)])
+      (define sample 
+        (inexact->exact 
+         (round (* s16max 0.1 (sin 
+                               (* i 1/44100 pitch 2 pi))))))
+      (s16vector-set! result (* i 2) sample)
+      (s16vector-set! result (add1 (* i 2)) sample))
+    result)
+  
   (define channels 2)
   (define s16max 32767)
   
@@ -66,30 +80,155 @@
 
   (define response-channel (make-channel))
   
-  ;; 1/2 second of 330
-  (define tone-buf-330 (make-s16vector (* 22050 channels)))
-  (for ([i (in-range 0 22050)])
-    (define sample (inexact->exact (round (* s16max 0.1 (sin (* i 1/44100 330 2 pi))))))
-    (s16vector-set! tone-buf-330 (* i 2) sample)
-    (s16vector-set! tone-buf-330 (add1 (* i 2)) sample))
+  (define (check-for-finished)
+    (check-for-finished/n 1))
+  
+  ;; check that there are 'n' 'finished' symbols waiting in 
+  ;; the response channel.
+  (define (check-for-finished/n n)
+    (for ([i (in-range n)])
+      (check-equal? (channel-try-get response-channel) 'finished))
+    (check-equal? (channel-try-get response-channel) #f))
+  
+  (define (test-start) 
+    (sleep 2)
+    (check-equal? (channel-try-get response-channel) #f)
+    (printf "starting now... "))
+  
+  (define (test-end)
+    (printf "... ending now.\n")
+    (sleep 1))
+  
+    
+  (define tone-buf-330 (make-tone-buf 330 22050))
+  
+  ;; first test with the vector interface:
   (let ()
     (define abort-box (box #f))
     (define callback
-      (make-copying-callback (s16vector->cpointer tone-buf-330)
-                             22050
-                             response-channel abort-box))
+      (make-copying-callback tone-buf-330
+                             response-channel
+                             abort-box))
     (define stream (open-test-stream callback))
     (printf "1/2 second @ 330 Hz\n")
-    (sleep 2)
-    (printf "starting now... ")
+    (test-start)
     (pa-start-stream stream)
     (sleep 0.5)
-    (printf "...ending now.\n")
-    (sleep 2))
+    (test-end)
+    (check-for-finished))
+
+  ;; second test with the cpointer interface:
+  (let ()
+    (define abort-box (box #f))
+    (define callback
+      (make-copying-callback/cpointer
+       (s16vector->cpointer tone-buf-330)
+       22050
+       response-channel
+       abort-box))
+    (define stream (open-test-stream callback))
+    (printf "1/2 second @ 330 Hz\n")
+    (test-start)
+    (pa-start-stream stream)
+    (sleep 0.5)
+    (test-end)
+    (check-for-finished))
   
+  (define tone-buf-380 (make-tone-buf 380 22050))
   
+  ;; two simultaneous streams, 330 & 380 Hz
+  (let ()
+    (define stream-1 (open-test-stream 
+                      (make-copying-callback
+                       tone-buf-330
+                       response-channel 
+                       (box #f))))
+    (define stream-2 (open-test-stream
+                      (make-copying-callback 
+                       tone-buf-380
+                       response-channel 
+                       (box #f))))
+    (printf "1/2 second @ 330 & 380 Hz\n")
+    (test-start)
+    (pa-start-stream stream-1)
+    (pa-start-stream stream-2)
+    (sleep 0.5)
+    (test-end)
+    (check-for-finished/n 2))
   
+  ;; ending a stream with the abort-box
+  (let ()
+    ;; a 10-second tone
+    (define abort-box (box #f))
+    (define longer-tone-buf (make-tone-buf 440 441000))
+    (define stream-1 (open-test-stream 
+                      (make-copying-callback
+                       longer-tone-buf
+                       response-channel 
+                       abort-box)))
+    (printf "1/2 second @ 440 Hz\n")
+    (test-start)
+    (pa-start-stream stream-1)
+    (sleep 0.5)
+    (set-box! abort-box #t)
+    (test-end)
+    (check-for-finished))
   
+  ;; GENERATING CALLBACKS
+  (let ()
+    (define abort-box (box #f))
+    (define (signal t)
+      (* 0.1 (sin (* t 1/44100 2 pi 410))))
+    (define callback
+      (make-generating-callback signal 1000 response-channel abort-box))
+    (define stream (open-test-stream callback))
+    (printf "1/2 second @ 410 Hz\n")
+    (test-start)
+    (pa-start-stream stream)
+    (sleep 0.5)
+    (set-box! abort-box #t)
+    (test-end)
+    (check-for-finished))
+  
+  ;; check for wrong size buffer
+  
+  (let ()
+    (define abort-box (box #f))
+    (define (signal t)
+      (* 0.1 (sin (* t 1/44100 2 pi 410))))
+    (define callback
+      ;; wrong buffer length:
+      (make-generating-callback signal 2000 response-channel abort-box))
+    (define stream (open-test-stream callback))
+    (pa-start-stream stream)
+    (sleep 0.25)
+    ;; should auto-abort:
+    (check-pred (lambda (exn) (and (exn:fail? exn)
+                                   (regexp-match #px"callback wanted 1000 frames"
+                                                 (exn-message exn))))
+                (channel-try-get response-channel))
+    (check-equal? (channel-try-get response-channel) #f))
+  
+  ;; check for signal that fails
+  (let ()
+    (define abort-box (box #f))
+    (define (signal t)
+      (error 'signal "I'm a bad signal."))
+    (define callback
+      (make-generating-callback signal 1000 response-channel abort-box))
+    (define stream (open-test-stream callback))
+    (pa-start-stream stream)
+    (sleep 0.25)
+    ;; should auto-abort:
+    (check-pred (lambda (exn) 
+                  (and (exn:fail? exn)
+                       (regexp-match #px"I'm a bad signal"
+                                     (exn-message exn))))
+                (channel-try-get response-channel))
+    (check-equal? (channel-try-get response-channel) #f))
+  
+  ;; check for signal that stalls
+  3
   )))
 
 #|;; multiple streams? 
