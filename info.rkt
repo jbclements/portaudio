7ae825408b06f52780166b228a538f70b8eb2bec
John Clements <clements@racket-lang.org>
2014-09-10 13:31:29 -0700

    add deps, update README

--- info.rkt
+++ info.rkt
@@ -2,3 +2,11 @@
 
 (define collection 'multi)
 (define version "0.1") ; version bump for add of s16vec-record
+
+(define deps
+  (list "base"))
+
+(define build-deps
+  (list "rackunit-lib"
+        "scribble-lib"))
+
