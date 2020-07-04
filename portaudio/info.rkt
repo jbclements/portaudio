d1f41b1915e42a09868fbe979d543f0c4d687aec
John Clements <clements@racket-lang.org>
2020-07-04 12:43:06 -0400

    remove leftover unnecessary info.rkt fields

--- portaudio/info.rkt
+++ portaudio/info.rkt
@@ -2,22 +2,8 @@
 
 (define name "PortAudio")
 
-(define blurb '((p "PortAudio is a cross-platform library "
-                   "for audio output and input. It runs "
-                   "on Windows, Mac OS X, and linux. "
-                   "This package provides Racket bindings "
-                   "for these functions. For higher-level "
-                   "tools and utilities, use the RSound package.")))
-
 (define scribblings '(("portaudio.scrbl" () (tool))))
 (define categories '(media))
-(define version "2020-05-13")
 (define release-notes '((p "moving dylibs to other packages")))
 (define compile-omit-paths '("test"))
-
-
-
-;; planet-specific:
-(define repositories '("4.x"))
-(define primary-file "main.rkt")
 
